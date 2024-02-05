9c4c5a33bd8f6ca89ee57d71a39e2650664345a2
ShaharGdn <shahargadon@gmail.com>
2024-02-05 19:36:00 +0200

    Add more books

--- js/services/bookshop.service.js
+++ js/services/bookshop.service.js
@@ -32,7 +32,7 @@ function addBook(elInput) {
     }
 
     gBooks.unshift(book)
-    
+
     _saveBooks()
     return book
 }
@@ -41,7 +41,7 @@ function removeBook(bookId) {
     // Delete
     const idx = gBooks.findIndex(book => book.id === bookId)
     gBooks.splice(idx, 1)
-    
+
     _saveBooks()
 }
 
@@ -74,12 +74,20 @@ function _createBooks() {
     gBooks = loadFromStorage('booksDB')
     if (!gBooks || gBooks.length === 0) {
         gBooks = [
-            // { name: 'Black Fog', author: 'Jane Air', price: '100', id: makeId(), imgURL: `<img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg">` },
-            // { name: 'Ginger', author: 'Shlomo', price: '120', id: makeId(), imgURL: `<img src="https://content.wepik.com/statics/90897927/preview-page0.jpg">` },
-            // { name: 'Matski', author: 'Ingi', price: '80', id: makeId(), imgURL: `<img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/art-book-cover-design-template-34323b0f0734dccded21e0e3bebf004c_screen.jpg?ts=1637015198">` },
             _createBook('Black Fog', 'Jane Air', '100', `<img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg">`),
             _createBook('Ginger', 'Shlomo', '120', `<img src="https://content.wepik.com/statics/90897927/preview-page0.jpg">`),
-            _createBook('Matski', 'Ingi', '80', `<img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/art-book-cover-design-template-34323b0f0734dccded21e0e3bebf004c_screen.jpg?ts=1637015198">`)
+            _createBook('Matski', 'Ingi', '80', `<img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/art-book-cover-design-template-34323b0f0734dccded21e0e3bebf004c_screen.jpg?ts=1637015198">`),
+            _createBook('The Secret Garden', 'Frances Hodgson Burnett', '150', '<img src="https://images.unsplash.com/photo-1588369821874-53c00efb7685">'),
+            _createBook('Pride and Prejudice', 'Jane Austen', '200', '<img src="https://images.unsplash.com/photo-1568534678241-1b2e5185c5c6">'),
+            _createBook('The Great Gatsby', 'F. Scott Fitzgerald', '180', '<img src="https://images.unsplash.com/photo-1600608104861-9ffbd2138e7c">'),
+            _createBook('To Kill a Mockingbird', 'Harper Lee', '120', '<img src="https://images.unsplash.com/photo-1566504740002-1a89e9ff183c">'),
+            _createBook('1984', 'George Orwell', '130', '<img src="https://images.unsplash.com/photo-1588591798329-d79a9fad52f2">'),
+            _createBook('The Catcher in the Rye', 'J.D. Salinger', '140', '<img src="https://images.unsplash.com/photo-1587402307869-00f786ef4911">'),
+            _createBook('Lord of the Flies', 'William Golding', '110', '<img src="https://images.unsplash.com/photo-1602039200700-fc4582eadd81">'),
+            _createBook('The Hobbit', 'J.R.R. Tolkien', '170', '<img src="https://images.unsplash.com/photo-1566567159379-27a24e3e7118">'),
+            _createBook('Brave New World', 'Aldous Huxley', '160', '<img src="https://images.unsplash.com/photo-1558208213-42b1b4e7662a">'),
+            _createBook('The Chronicles of Narnia', 'C.S. Lewis', '190', '<img src="https://images.unsplash.com/photo-1583030725685-4a87b0d57892">')
+
         ]
         _saveBooks()
     }
